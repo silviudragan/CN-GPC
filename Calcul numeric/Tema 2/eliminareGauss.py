5baa9124619a85f746bbf7fc356ce5dbb7be81d1
Dragan Silviu <silviu.dragan@gmail.com>
2018-03-10 13:42:00 +0200

    mandatory part

--- Calcul numeric/Tema 2/eliminareGauss.py
+++ Calcul numeric/Tema 2/eliminareGauss.py
@@ -1,5 +1,8 @@
-from math import fabs
+from math import fabs, sqrt
 from pprint import pprint
+import copy
+import numpy as np
+
 global dimensiune
 global matrix
 global precizie
@@ -15,7 +18,7 @@ def citire_date():
     dimensiune = f.readline()
     precizie = f.readline()
     dimensiune = int(dimensiune)
-    precizie = 1/pow(10, int(precizie))
+    precizie = 1 / pow(10, int(precizie))
     matrix = []
     it = dimensiune
     while it:
@@ -26,6 +29,7 @@ def citire_date():
     # pprint(matrix1)
     rezultat = [int(i) for i in f.readline().split()]
     # print(rezultat)
+
 
 citire_date()
 
@@ -52,27 +56,22 @@ def max_coloana(extinsa, coloana):
 
 def rezolva_sistem_superior_triunghiular(extinsa):
     solution = []
-    for i in range(dimensiune-1, -1, -1):
+    for i in range(dimensiune - 1, -1, -1):
         it_solution = 0
         x = 0
-        for j in range(dimensiune-1, i, -1):
-            x += solution[it_solution]*extinsa[i][j]
+        for j in range(dimensiune - 1, i, -1):
+            x += solution[it_solution] * extinsa[i][j]
             it_solution += 1
-        solution.append((extinsa[i][dimensiune] - x)/extinsa[i][i])
+        solution.append((extinsa[i][dimensiune] - x) / extinsa[i][i])
     rez = dict()
-    for i in range(1, dimensiune+1):
-        key = 'x' + str(i)
-        rez[key] = solution[i-1]
-    return rez
-
-
-def verifica_solutie():
-    pass
-    # de implementat
+    for i in reversed(range(1, dimensiune + 1)):
+        key = 'x' + str(dimensiune-i+1)
+        rez[key] = solution[i - 1]
+    return rez, list(reversed(solution))
 
 
 def substitutie_inversa():
-    extinsa = matrix
+    extinsa = copy.deepcopy(matrix)
     for i in range(dimensiune):
         extinsa[i].append(rezultat[i])
 
@@ -83,10 +82,10 @@ def substitutie_inversa():
     # interschimbarea celor 2 linii, daca este nevoie
     extinsa[index_pivot], extinsa[l] = extinsa[l], extinsa[index_pivot]
     while l < dimensiune - 1 and fabs(au) > precizie:
-        for i in range(l+1, dimensiune):
-            f = extinsa[i][l]/extinsa[l][l]
-            for j in range(l+1, dimensiune+1):
-                extinsa[i][j] = extinsa[i][j] - f*extinsa[l][j]
+        for i in range(l + 1, dimensiune):
+            f = extinsa[i][l] / extinsa[l][l]
+            for j in range(l + 1, dimensiune + 1):
+                extinsa[i][j] = extinsa[i][j] - f * extinsa[l][j]
             extinsa[i][l] = 0
         l += 1
         index_pivot = pivot(extinsa, l)
@@ -97,8 +96,19 @@ def substitutie_inversa():
     if fabs(au) <= precizie:
         print("matrice singulara")
     else:
-        solution = rezolva_sistem_superior_triunghiular(extinsa)
-        print(solution)
-        verifica_solutie()
-    # pprint(extinsa)
+        solution, raw = rezolva_sistem_superior_triunghiular(extinsa)
+        print("Solutia sistemului este ", solution)
+
+        ##################################################################
+        ##################### Verificare solutie #########################
+        ##################################################################
+        verificare_sol = np.dot(matrix, raw) - rezultat
+        print("Verificarea solutiei ", verificare_sol)
+        sum = 0
+        for i in verificare_sol:
+            sum += pow(fabs(i), 2)
+        print("|| z || = ", sqrt(sum))
+        # pprint(extinsa)
+
+
 substitutie_inversa()
