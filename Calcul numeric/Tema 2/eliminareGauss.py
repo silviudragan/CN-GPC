c0b34aac616def9f75522999e9870a2134050a05
Dragan Silviu <silviu.dragan@gmail.com>
2018-03-12 15:02:26 +0200

    asd

--- Calcul numeric/Tema 2/eliminareGauss.py
+++ Calcul numeric/Tema 2/eliminareGauss.py
@@ -31,13 +31,10 @@ def citire_date():
     # print(rezultat)
 
 
-citire_date()
-
-
 def pivot(extinsa, l):
     max = fabs(extinsa[l][0])
     index = 0
-    for i in range(1, dimensiune):
+    for i in range(l, dimensiune):
         if max < fabs(extinsa[i][l]):
             max = fabs(extinsa[i][l])
             index = i
@@ -60,9 +57,14 @@ def rezolva_sistem_superior_triunghiular(extinsa):
         it_solution = 0
         x = 0
         for j in range(dimensiune - 1, i, -1):
+            print(i, j, extinsa[i][j])
             x += solution[it_solution] * extinsa[i][j]
             it_solution += 1
-        solution.append((extinsa[i][dimensiune] - x) / extinsa[i][i])
+        print(i, extinsa[i][i])
+        try:
+            solution.append((extinsa[i][dimensiune] - x) / extinsa[i][i])
+        except Exception:
+            pass
     rez = dict()
     for i in reversed(range(1, dimensiune + 1)):
         key = 'x' + str(dimensiune-i+1)
@@ -78,7 +80,7 @@ def substitutie_inversa():
     l = 0
     index_pivot = pivot(extinsa, l)
     au = extinsa[index_pivot][l]
-
+    print()
     # interschimbarea celor 2 linii, daca este nevoie
     extinsa[index_pivot], extinsa[l] = extinsa[l], extinsa[index_pivot]
     while l < dimensiune - 1 and fabs(au) > precizie:
@@ -107,8 +109,120 @@ def substitutie_inversa():
         sum = 0
         for i in verificare_sol:
             sum += pow(fabs(i), 2)
-        print("|| z || = ", sqrt(sum))
+        print("|| z ||2 = ", sqrt(sum))
         # pprint(extinsa)
 
 
-substitutie_inversa()
+
+        sol_num = np.linalg.solve(matrix, rezultat)
+        # print("linalg ", sol_num)
+        ver_sol_num = np.dot(matrix, sol_num) - rezultat
+        #print("ver sol num ", ver_sol_num)
+        sum = 0
+        for i in ver_sol_num:
+            sum += pow(fabs(i), 2)
+        print("Verificara solutiei cu ajutorul librariei:")
+        print("|| A* Xgauss - b ||2 = ", sqrt(sum))
+
+
+def main_mandatory():
+    citire_date()
+    substitutie_inversa()
+
+main_mandatory()
+
+global a
+global b
+global c
+
+
+def citire_bonus():
+    global a
+    global b
+    global c
+    global precizie
+    global rezultat
+    global dimensiune
+    f = open("input.txt", 'r')
+    dimensiune = f.readline()
+    precizie = f.readline()
+    dimensiune = int(dimensiune)
+    precizie = 1 / pow(10, int(precizie))
+    a = []
+    b = []
+    c = []
+    it = dimensiune
+    pas = 0
+    while it:
+        line = f.readline().split()
+        int_line = [int(i) for i in line]
+        if pas != 0:
+            c.append(int_line[pas-1])
+        a.append(int_line[pas])
+        try:
+            b.append(int_line[pas+1])
+        except Exception:
+            pass
+        pas += 1
+        it -= 1
+    rezultat = [int(i) for i in f.readline().split()]
+    # print(a, b, c, rezultat)
+
+global d
+global e
+global f
+
+
+def zero_sub_dig():
+    global d
+    global e
+    global f
+    d = []
+    e = []
+    f = []
+    for i in range(dimensiune-1):
+        if fabs(c[i]) < fabs(a[i]):
+            a[i+1] = a[i+1] - c[i]/a[i]*b[i]
+            d.append(a[i])
+            e.append(b[i])
+            f.append(0)
+        else:
+            a[i+1] = -1 * a[i]/c[i] * a[i+1] + b[i]
+            try:
+                b[i+1] = 0
+            except Exception:
+                pass
+            d.append(c[i])
+            e.append(a[i+1])
+            try:
+                f.append(b[i+1])
+            except Exception:
+                pass
+    d.append(a[dimensiune-1])
+    print(d, e, f)
+
+
+def rezolvare_sistem():
+    solution = [rezultat[dimensiune-1]/d[dimensiune - 1]]
+    solution.append((rezultat[dimensiune-2] - e[dimensiune - 2]*solution[0])/d[dimensiune-2])
+
+    pas = 0
+    for i in range(dimensiune-3, -1, -1):
+        x = dimensiune - pas - 1
+        sum = 0
+        sum += f[i]*rezultat[x]
+        sum += e[i]*rezultat[x-1]
+        sum += d[i]*rezultat[x-2]
+        pas += 1
+        solution.append(sum)
+    return solution
+
+
+def main_bonus():
+    citire_bonus()
+    zero_sub_dig()
+    solutie = rezolvare_sistem()
+    print(solutie)
+
+
+main_bonus()
